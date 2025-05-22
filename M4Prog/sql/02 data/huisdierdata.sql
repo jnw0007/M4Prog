6dbcbf57d737e6f1a677a4e0340e80763a5fcff7
jnw0007 <38678@ma-web.nl>
2025-05-22 12:32:18 +0200

    les02

---
+++ M4Prog/sql/02 data/huisdierdata.sql
@@ -0,0 +1,8 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+(NULL, 'fikkie', 'niet ik', 'hond', '14', '2025-05-07'),
+(NULL, 'flappie', 'melissa', 'paard', '98', '2024-06-12'),
+(NULL, 'grrr', 'nogsteeds niet ik', 'cavia', '1', '2023-01-04'),
+(NULL, 'mori', 'penny', 'kat', '7', '2021-05-10'),
+(NULL, 'gurt', 'kevin', 'hamster', '1', '2024-10-05');
+
